c1df4a321af97c105a1048fde1abb41acb823a95
hanad-alm-d <102107738@student.swin.edu.au>
2024-11-01 11:47:44 +1100

    create header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/Signin";
 import About from "./pages/about";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
