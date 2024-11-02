03ad602f8371d6ce2c5408347ae9a1fc88a53b44
hanad-alm-d <102107738@student.swin.edu.au>
2024-11-03 10:12:07 +1100

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -22,4 +23,7 @@ app.get("/", (req, res) => {
   res.send("helo work");
 });
 
+app.use(express.json());
+
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
