bf7f97b3fde73c03f3b79b8cec6fe60d36411061
hanad-alm-d <102107738@student.swin.edu.au>
2024-11-03 13:18:36 +1100

    complete sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,12 +5,11 @@ import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
-
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
-    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
     res.status(201).json("user created");
   } catch (error) {
-    next(errorHandler(550, "error from the function"));
+    next(error);
   }
 };
