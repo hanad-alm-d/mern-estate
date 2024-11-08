8cd45f1324b1521f060be81a19614548ecf5e7cd
hanad-alm-d <102107738@student.swin.edu.au>
2024-11-08 19:45:29 +1100

    addd redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
-import useReducer from "./user/userSlice.js";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice.js"; // Corrected: Changed `useReducer` to `userReducer`
+import { persistReducer, persistStore } from "redux-persist"; // Added `persistStore`
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: useReducer },
+  reducer: persistedReducer, // Removed `{ user: persistedReducer }` and directly used `persistedReducer`
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store); // Corrected: Changed `persistorStore` to `persistStore`
