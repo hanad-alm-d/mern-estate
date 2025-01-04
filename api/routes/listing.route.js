a1a5797447c707230a62a78a650a77ef69c33a2e
hanad-alm-d <102107738@student.swin.edu.au>
2025-01-04 17:04:33 +1100

    Show UserListings API

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,6 +2,8 @@ import express from "express";
 import {
   createListing,
   getListing,
+  getListings,
+  getUserListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,6 +13,7 @@ router.post("/create", verifyToken, createListing);
 //router.delete('/delete/:id', verifyToken, deleteListing);
 //router.post('/update/:id', verifyToken, updateListing);
 router.get("/get/:id", getListing);
-//router.get('/get', getListings);
+router.get("/get", getListings);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
