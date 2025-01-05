9aaf21d6e76a564864b254a32f259313f1f7b8ab
hanad-alm-d <102107738@student.swin.edu.au>
2025-01-05 15:23:52 +1100

    Update Listing API

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,6 +5,7 @@ import {
   getListings,
   getUserListings,
   deleteListing,
+  updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -12,7 +13,7 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
-//router.post('/update/:id', verifyToken, updateListing);
+router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing);
 router.get("/get", getListings);
 router.get("/listings/:id", verifyToken, getUserListings);
